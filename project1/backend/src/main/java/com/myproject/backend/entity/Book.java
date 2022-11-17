e81fb824366328480a8150cb1a8dfb078993413c
omardbaa <dbaaomar18@gmail.com>
2022-11-17 17:25:39 +0100

    ignore it

--- project1/backend/src/main/java/com/myproject/backend/entity/Book.java
+++ project1/backend/src/main/java/com/myproject/backend/entity/Book.java
@@ -3,6 +3,8 @@ package com.myproject.backend.entity;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 public
 
@@ -17,6 +19,9 @@ public
 class Book {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
+    @Size(min = 0, max = 20)
     private  String title;
 
 
