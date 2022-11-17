e3c0a1bb0827c5026f40038b1bc709662fa110d9
abderrahim elouardy <elouardyabderrahim07@gmail.com>
2022-11-17 17:24:30 +0100

    swagger config with annotation and programmatecly

--- project1/backend/src/main/java/com/myproject/backend/SwaggerConfig.java
+++ project1/backend/src/main/java/com/myproject/backend/SwaggerConfig.java
@@ -1,12 +1,23 @@
 package com.myproject.backend;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
 
+
+@Configuration
+//@Swagger
 public class SwaggerConfig {
 
+ /*   @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }*/
 
 
 }
--- project1/backend/src/main/java/com/myproject/backend/controller/BookController.java
+++ project1/backend/src/main/java/com/myproject/backend/controller/BookController.java
@@ -2,6 +2,12 @@ package com.myproject.backend.controller;
 
 import com.myproject.backend.entity.Book;
 import com.myproject.backend.repository.BookRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -10,12 +16,27 @@ import java.util.Collection;
 import java.util.Optional;
 @RestController
 @RequestMapping("/api/book")
+@Tag(name="books" , description = "hhhhh")
 public class BookController {
 
 
     @Autowired
     private BookRepository repository;
 
+
+    @Operation(summary = "Get a book by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the book",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Book.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied",
+                    content = @Content),
+            @ApiResponse(responseCode = "401", description = "Unauthorized",
+            content = @Content),
+            @ApiResponse(responseCode = "403", description = "forbidden",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Book not found",
+                    content = @Content) })
     @GetMapping("/{id}")
     public Optional<Book> findById(@PathVariable long id) {
         return repository.findById(id);
